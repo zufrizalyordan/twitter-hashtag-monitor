1a247475a40a8c4c30bbc588d8afe5839baba79d
zufrizal yordan <zufrizalyordan@gmail.com>
2013-09-20 22:28:49 +0700

    getting values from twitter stream using ntwitter and show it to node js console.

---
+++ app.js
@@ -0,0 +1,23 @@
+var twitter = require('ntwitter');
+var credentials = require('./credentials.js');
+
+var t = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+});
+
+var track_item = '#android';
+t.stream(
+  'statuses/filter',
+  { track: [track_item] },
+  function(stream) {
+    stream.on('data', function(tweet) {
+        //if #hashtag is in the tweet text, emit via socket io
+        if(tweet.text.match(track_item)) {
+            console.log(tweet.text);
+        }
+    });
+  }
+);
