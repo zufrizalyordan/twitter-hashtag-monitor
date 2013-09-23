800f55367186b3d31903091413ca663d625b4f90
zufrizal yordan <zufrizalyordan@gmail.com>
2013-09-24 00:21:07 +0700

    call socket io and initialize

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var twitter = require('ntwitter'),
     credentials = require('./credentials.js'),
     express = require('express'),
     app = express(),
-    server = require('http').createServer(app);
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
 var t = new twitter({
     consumer_key: credentials.consumer_key,
@@ -17,16 +18,20 @@ app.get('/', function (req, res) {
 
 server.listen(3000);
 
-var track_item = '#android';
-t.stream(
-  'statuses/filter',
-  { track: [track_item] },
-  function(stream) {
-    stream.on('data', function(tweet) {
-        //if #hashtag is in the tweet text, emit via socket io
-        if(tweet.text.match(track_item)) {
-            console.log(tweet.text);
-        }
-    });
-  }
-);
+io.sockets.on('connection', function (socket) {
+
+  var track_item = '#android';
+  t.stream(
+    'statuses/filter',
+    { track: [track_item] },
+    function(stream) {
+      stream.on('data', function(tweet) {
+          //if #hashtag is in the tweet text, emit via socket io
+          if(tweet.text.match(track_item)) {
+              console.log(tweet.text);
+          }
+      });
+    }
+  );
+
+});
