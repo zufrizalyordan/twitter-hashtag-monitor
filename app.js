06b60d2161f822081595319773ae26912355ba58
Yunita Anggraeni <yunitaanggraeni@mbpr-ag.local>
2017-08-17 17:06:57 +0700

    use twitter module to replace nTwitter

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var twitter = require('ntwitter'),
+var twitter = require('twitter'),
   	credentials = require('./credentials.js'),
   	express = require('express'),
   	app = express(),
@@ -13,25 +13,28 @@ var t = new twitter({
 });
 
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000);
 
 io.sockets.on('connection', function (socket) {
-
 	console.log('SOCKET CONNECTED\n');
-	var track_item = '#zy';
-	t.stream(
-		'statuses/filter',
-		{ track: [track_item] },
-		function(stream) {
-			stream.on('data', function(tweet) {
-				if(tweet.text.match(track_item)) {
-					socket.emit('tweets', { detail: tweet });
-				}
-			});
-		}
-	);
+	var track_item = '#RI72';
+  
+  var twitterStream = t.stream(
+    'statuses/filter',
+    { track: track_item }
+  );	
+
+  twitterStream.on('data', function(tweet) {
+    if(tweet && tweet.text.match(track_item)) {
+      socket.emit('tweets', { detail: tweet });
+    }
+  });
+
+  twitterStream.on('error', function(error) {
+    throw error;
+  });
 
 });
