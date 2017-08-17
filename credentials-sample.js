b75cddc3ac2a51a00419802970979ab40d45a530
Yunita Anggraeni <yunitaanggraeni@mbpr-ag.local>
2017-08-17 17:07:15 +0700

    update credentials sample

--- credentials-sample.js
+++ credentials-sample.js
@@ -1,11 +1,6 @@
-var credentials = {
+module.exports = {
     "consumer_key": TWITTER_CONSUMER_KEY,
     "consumer_secret": TWITTER_CONSUMER_SECRET,
     "access_token_key": TWITTER_ACCESS_TOKEN_KEY,
     "access_token_secret": TWITTER_ACCESS_TOKEN_SECRET,
 }
-
-module.exports = {
-    credentials: credentials,
-};
-
